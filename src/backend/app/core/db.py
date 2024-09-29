064ed4e1c9ef13618bcf375784a8e6e919029669
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2024-09-29 15:57:10 -0300

    :bug: Fix database initialization

--- src/backend/app/core/db.py
+++ src/backend/app/core/db.py
@@ -1,11 +1,18 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 from app.core.config import settings
 
 DATABASE_URL = settings.DATABASE_URL
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+Base = declarative_base()
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
 
 async def get_db():
--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
-from app.api.v1.endpoints import predictions
+from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+from app.api.v1.endpoints import predictions
+from app.core.db import init_db
+
+
+@asynccontextmanager
+async def app_lifespan(app: FastAPI):
+    await init_db()
+    print("Application startup")
+
+    yield
+    print("Application shutdown")
+
+
+app = FastAPI(lifespan=app_lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(predictions.router, prefix="/api/v1", tags=["predictions"])
 
